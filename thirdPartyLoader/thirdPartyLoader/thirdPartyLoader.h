279af5dbf02fd835db54ce24712d71776b757f3a
Souvik Maiti <souvik.maiti@robosoftin.com>
2022-10-09 15:09:33 +0530

    inital commit

---
+++ thirdPartyLoader/thirdPartyLoader/thirdPartyLoader.h
@@ -0,0 +1,18 @@
+//
+//  thirdPartyLoader.h
+//  thirdPartyLoader
+//
+//  Created by Souvik Maiti on 09/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for thirdPartyLoader.
+FOUNDATION_EXPORT double thirdPartyLoaderVersionNumber;
+
+//! Project version string for thirdPartyLoader.
+FOUNDATION_EXPORT const unsigned char thirdPartyLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <thirdPartyLoader/PublicHeader.h>
+
+
